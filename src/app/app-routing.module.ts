7d4793a01de63f4df669ca3e5f02f6000427f6db
xandyx2014 <xandyx2014@gmail.com>
2018-07-30 14:56:41 -0400

    comensando con los estilos

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { dashboardRoutes } from './dashboard/dashboard.routes';
+
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  {
+    path: '',
+    component: DashboardComponent,
+    children: dashboardRoutes
+  },
+  { path: '**', redirectTo: '' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
